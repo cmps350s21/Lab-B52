b7b9ed2d809546672a49b3a1f2253f6225186189
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-23 13:33:20 +0300

    Query
    Request query

--- Lab 9 Web API/BankingApp/app.js
+++ Lab 9 Web API/BankingApp/app.js
@@ -1,16 +1,40 @@
 import express from 'express'
+import fs from 'fs-extra'
 
 //port number
 const port = 3000
-
 const app = express()
 
 //two types [dynamic , static]
 app.use(express.static('public'))
+
 //web services [routes]
-app.get('/api/sayhi', (req, res)=>{
-    res.send('Welcome to the server side')
+//Reading
+app.get('/api/accounts', async (req, res)=>{
+    //query string
+    console.log(req)
+    const type = req.query.type
+    const accounts = await fs.readJson('./data/accounts.json')
+    let filtered = accounts.filter(acc=>acc.acctType == type)
+    res.json(filtered)
 })
+
+//creating , adding new instance/resource
+app.post('/api/accounts', (req, res)=>{
+    res.send('We create a new account')
+})
+
+//updating an existing resource
+app.put('/api/accounts', (req, res)=>{
+    res.send('We update the account')
+})
+
+//deleting , removing resource from the server
+app.delete('/api/accounts', (req, res)=>{
+    res.send('We will delete the account')
+})
+
+//CRUD operations on
 
 app.listen(port, ()=>{
     console.log(`Server started @http://localhost:${port}`)
